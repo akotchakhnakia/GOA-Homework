0d0330a75bbbe1e37111edca0d4d95c1b343124f
akotchakhnakia <167247429+akotchakhnakia@users.noreply.github.com>
2024-05-20 17:25:32 +0400

    Create homework.py
    commit

---
+++ day 03/homework.py
@@ -0,0 +1,36 @@
+
+
+
+#მშობლის და ჩემი ასაკის გამოთვლა (ნომერი 5)
+age1 = 22
+age2 = 42
+print(age1 - age2)
+
+
+#ჯერ სახელის დაბეჭდვა შემდეგ ასაკი (ნომერი 4)
+name = ("ako")
+age = (22)
+print(name)
+print(age)
+
+#არითმეტიკული მოქმედება (ნომერი 3)
+number1 = 15
+number2 = 25
+print(number1 + number2)
+
+
+#+10 წელი ყველას :D (ნომერი 2)
+ageako = 22
+agejuka = 42
+print(ageako + 10)
+print(agejuka + 10)
+
+
+#ფასი * რაოდენობაზე (ნომერი 1)
+thing = 10
+quantity = 5
+print(thing * quantity)
+
+
+
+
