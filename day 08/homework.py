c1c12d664bc856e6c667100ceba7c2220dd385ca
akotchakhnakia <167247429+akotchakhnakia@users.noreply.github.com>
2024-05-31 02:35:23 +0400

    commit
    commit

---
+++ day 08/homework.py
@@ -0,0 +1,48 @@
+
+
+
+#მეტია
+print(5 > 3)
+print(10 > 1)
+print(7 > 3)
+print(6 > 1)
+print(5 > 4)
+
+#ნაკლებია
+print(3 < 9)
+print(9 < 10)
+print(12 < 18)
+print(15 < 29)
+print(45 < 50)
+
+#ტოლია
+print(5 == 5)
+print(9 == 9)
+print(8 == 8)
+print(5 == 5)
+print(10 == 10)
+
+#მეტია ან ტოლია
+print(5 >= 4)
+print(7 >= 5)
+print(8 >= 3)
+print(10 >= 9)
+print(9 >= 1)
+
+#ნაკლებია ან ტოლია
+print( 9 <= 10)
+print(5 <= 8)
+print(7 <= 9)
+print(1 <= 5)
+print(2 <= 9)
+
+#არ უდრის
+print(6 != 7)
+print(4 != 8)
+print(5 != 9)
+print(6 != 8)
+print(1 != 2)
+
+
+
+
---
+++ day 08/homework2.py
@@ -0,0 +1,44 @@
+num1 = 5
+num2 = 6
+print(num1 >= num2)
+
+num1 = 9
+num2 = 4
+print(num1 <= num2) 
+
+num1 = 5
+num2 = 5
+print(True and False)
+
+num1 = 4
+num2 = 1
+print(num1 >= 2 and num2 <= 3) 
+
+num1 = 5
+num2 = 5
+print(num1 == num2)
+
+num1 = 9
+num2 = 8
+print(num1 <= 5 or num2 >= 5)
+
+num1 = 7
+num2 = 5
+print(num1 >= 10 or num2 <= 3)
+
+num1 = 99
+num2 = 100
+print(num1 >= 99 or num2 >= 100)
+
+num1 = 56
+num2 = 67
+print(num1 <= 57 or num2 >= 65)
+
+num1 = 10
+num2 = 5
+print(True or True)
+
+num1 = 89
+num2 = 98
+print(False or False) 
+
