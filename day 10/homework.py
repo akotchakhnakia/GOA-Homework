a1a9b5a631b4527052fcd756051196c6c0260a9e
akotchakhnakia <167247429+akotchakhnakia@users.noreply.github.com>
2024-06-09 22:29:19 +0400

    commit
    commit

---
+++ day 10/homework.py
@@ -0,0 +1,12 @@
+  
+#mteli ricxvebi
+for i in range(0,20): 
+  print(i)
+
+  #naturaluri ricxvebi 10 mde
+  for i in range(10):
+    print(i)
+
+    #luwi ricxvebi 100mde
+for i in range(2,100,2 ):
+    print(i)
---
+++ day 10/homework4.py
@@ -0,0 +1,17 @@
+num = int (input("enter number: "))
+if num == 1:
+    print("monday")
+elif num == 2:
+    print("thuesday")
+elif num == 3:
+    print("wensday")
+elif num == 4:
+    print("thuesday")
+elif num == 5:
+    print("friday")
+elif num == 6:
+    print("saturday")
+elif num == 7:
+    print("sunday")
+else:
+    print("invalid number")
