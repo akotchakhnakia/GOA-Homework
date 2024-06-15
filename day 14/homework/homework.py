c23b923e818e64dbf2365df50f15f91cbb3c3d07
akotchakhnakia <167247429+akotchakhnakia@users.noreply.github.com>
2024-06-16 01:14:29 +0400

    commit
    day 14

---
+++ day 14/homework/homework.py
@@ -0,0 +1,38 @@
+# num = 0
+# while num != 5:
+#    num = int (input("pleas enter number: "))
+#    if num == 5:
+#       print("you won")
+#    else:
+#       print("you lose")
+      
+
+#gavaketott shevdzlot
+# sum = 1
+# for i in range(1,10):
+
+#     sum = sum * i
+
+# print(sum)
+
+#shemdegi 
+# sum = 0
+# for i in range(1,10):
+#     sum = sum + i
+# print(sum)
+
+# for i in range(1,51):
+#     if i % 5 == 0:
+#         print(i)
+
+
+name = "ako"
+print(name[::-1])
+
+
+
+
+
+
+
+
